f77d011e5a1cb6bc20ec06ee84a902385a15ee75
Ren-Oliveira <ren.jun.oliveira@gmail.com>
2022-03-23 20:07:46 +0000

    setting up next steps

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -3,16 +3,25 @@ import { useTheme } from '../store/ThemeContext';
 import 'bulma/css/bulma.min.css';
 import ContactsCard from '../components/ui/resume/ContactsCard';
 
-const Contacts = props => {
+const Contacts = () => {
   const darkTheme = useTheme();
+
   const classDark = 'has-background-black-ter';
   const classLight = 'has-background-danger-light';
+
+  const textDark = 'has-text-danger-light has-text-centered p-2';
+  const textLight = 'has-text-success-dark has-text-centered p-2';
 
   return (
     <div className={darkTheme ? classDark : classLight}>
       <div className="hero is-fullheight">
         <div className="hero-body">
-          <ContactsCard />
+          <div className="container">
+            <div className="mt-5">
+              <ContactsCard />
+            </div>
+            <p className={darkTheme ? textDark : textLight}>Leave a message</p>
+          </div>
         </div>
       </div>
     </div>
